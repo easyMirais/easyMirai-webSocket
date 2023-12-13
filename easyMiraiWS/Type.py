506e8a76f0a89af08c6ef7c55435231d41989780
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-12-13 09:37:01 +0800

    fix: 类型修正传入格式

--- easyMiraiWS/Type.py
+++ easyMiraiWS/Type.py
@@ -1,19 +1,19 @@
 class MessageType:  # 信息类型
-    def plan(self, message: str):
-        return "plan"
+    def Plain(self, message: str):
+        return "Plain"
 
 
 class TargetType:  # 目标类型
 
-    def friend(self, target: int):
-        return "friend"
+    def Friend(self, target: int):
+        return "Friend"
 
 
 class ReturnType:  # 返回类型
     @property
     def Dict(self):
-        return "dict"
+        return "Dict"
 
     @property
-    def json(self):
+    def Json(self):
         return "json"
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from easyMiraiWS import targetType, messageType, returnType
 async def MiraiRun():  # 运行主类
     easy_mirai = Mirai(uri="localhost", port=8080, verifyKey="123456789", qq=123456789)
     # 向好友987654321发送文字消息hello world并返回一个Dict类型的值
-    message = await easy_mirai.send(targetType.friend(987654321), messageType.plan("Hello World"), returnType.Dict)
+    message = await easy_mirai.send(targetType.Friend(987654321), messageType.Plain("Hello World"), returnType.Dict)
     print(message)
     pass
 
