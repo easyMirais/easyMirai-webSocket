f12f7bbfbb2ebee3caa3db62e9a41546d99b77ec
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2024-01-27 14:38:43 +0800

    fix: 更新排除文件

--- main.py
+++ main.py
@@ -2,21 +2,10 @@
 # -*- coding: utf-8 -*-
 # @Author    : SFNCO-Studio
 # @Time      : 2023/5/24 20:31
-# @File      : main.py
+# @File      : kit.py
 # @Project   : easyMirai-webSocket
 # @Uri       : https://sfnco.com.cn/
-import easyMiraiWS
-from easyMiraiWS import Mirai
-from easyMiraiWS import targetType, messageType, returnType
-
-
-async def MiraiRun():  # 运行主类
-    easy_mirai = Mirai(uri="localhost", port=8080, verifyKey="123456789", qq=123456789)
-    # 向好友987654321发送文字消息hello world并返回一个Dict类型的值
-    message = await easy_mirai.send(targetType.Friend(987654321), messageType.Plain("Hello World"), returnType.Dict)
-    print(message)
-    pass
-
+from easyMirais import Mirai
 
 if __name__ == '__main__':
-    easyMiraiWS.run(MiraiRun)
+    Mirai().startServer()
