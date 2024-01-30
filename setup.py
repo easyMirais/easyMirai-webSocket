20276e53d62eb45a9d6a54b820161e15c9c914cd
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2024-01-30 10:12:12 +0800

    ci: 添加自动打包程序

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easyMirai-websocket",
+    version="1.0.39A",
+    author="HexMikuMax & ExMikuPro",
+    author_email="sfnco-miku@outlook.com",
+    python_requires=">=3.6.0",
+    packages=find_packages(),
+    license="GPLv3",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
